486ba5c9eea8745df3b35a3142e991d7d043db74
LUU DUONG <luudinhduong2806@gmail.com>
2022-05-05 16:51:37 +0700

    fix laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('users', [UserController::class, 'index'])->name('users.list');
+Route::get('/users', 'UsersController@index')->name('index');
