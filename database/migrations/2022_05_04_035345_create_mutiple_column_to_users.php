567e394bd5a37568bb1df7b8e070821365489060
LUU DUONG <luudinhduong2806@gmail.com>
2022-05-16 13:18:54 +0700

    fix laravel basic 1

--- database/migrations/2022_05_04_035345_create_mutiple_column_to_users.php
+++ database/migrations/2022_05_04_035345_create_mutiple_column_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddMutipleColumnToUsers extends Migration
+class CreateMutipleColumnToUsers extends Migration
 {
     /**
      * Run the migrations.
